c44aa552c3be749976da5b3b5d176d8ad3117372
Shawn Anderson <swanders@cs.ualberta.ca>
2014-01-16 14:58:36 -0700

    new constructors

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -1,7 +1,43 @@
 package ca.ualberta.cs.lonelytwitter;
-
+import java.util.Date;
 
 public class LonelyTweetModel
 {
+	private String text;
+	private Date timestamp;
+	
+	public LonelyTweetModel(String text)
+	{
+		super();
+		this.text = text;
+		timestamp = new Date();}
+
+	public LonelyTweetModel(String text, Date timestamp)
+	{
+		super();
+		this.text = text;
+		this.timestamp = timestamp;}
+	
+	
+	public String getText()
+	{
+	return text;}
+	
+	
+	public void setText(String text)
+	{
+	this.text = text;}
+	
+	
+	public Date getTimestamp()
+	{
+	return timestamp;}
+	
+	
+
+	public void setTimestamp(Date timestamp)
+	{
+	this.timestamp = timestamp;}
+	
 
 }
