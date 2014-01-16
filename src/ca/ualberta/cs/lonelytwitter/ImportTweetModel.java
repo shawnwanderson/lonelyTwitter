16b98ba98896229a16af1b44f7d337ccd2a6cffb
Shawn Anderson <swanders@cs.ualberta.ca>
2014-01-16 15:14:59 -0700

    added the subclass tweet model

---
+++ src/ca/ualberta/cs/lonelytwitter/ImportTweetModel.java
@@ -0,0 +1,31 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class ImportTweetModel extends LonelyTweetModel
+{
+
+	public ImportTweetModel(String text, Date timestamp)
+	{
+
+		super(text, timestamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImportTweetModel(String text)
+	{
+
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getText() {
+		return "important! " + super.getText();
+	}
+	}
+
+	
+	
+	
+
